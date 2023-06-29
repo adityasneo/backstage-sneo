32443a7f53b4cf6b5b74f306f0e180ca1692092c
adityasneo <128022129+adityasneo@users.noreply.github.com>
2023-06-29 17:49:33 +0530

    Update App.tsx

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -33,9 +33,26 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPage } from '@backstage/core-components';
 
 const app = createApp({
   apis,
+  components: {
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        auto
+        provider={{
+          id: 'microsoft-auth-provider',
+          title: 'microsoft',
+          message: 'Sign in using microsoft',
+          apiRef: microsoftAuthApiRef,
+        }}
+      />
+    ),
+  },
+  
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
